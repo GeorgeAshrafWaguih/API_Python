a1c84dcf0a707c7ae737847772780f82f0065494
George <George.ashraf@hotmail.com>
2023-01-02 15:54:14 +0200

    Parsing the JSON response

--- test/sample_test1.py
+++ test/sample_test1.py
@@ -1,9 +1,17 @@
 import requests
 import json
 
+# Simple API Test
 BASE_URL = "https://reqres.in"
+
+# Adding Query Parameter
 params = {'page': 2}
 response = requests.get(BASE_URL + "/api/users", params=params)
 print(response)
 print(response.json())
 print(json.dumps(response.json(), indent=4))
+
+# Parsing the JSON response
+resp = response.json()
+first_names = [d['first_name'] for d in resp["data"]]
+print(first_names)
